1def875d3f392ca9cfa192960d1923866e4e0498
niom1 <63248775+niom1@users.noreply.github.com>
2021-01-22 14:14:22 +0100

    Add files via upload

---
+++ Javascript LB02/json/data.js
@@ -0,0 +1,46 @@
+const professionList = ["Applikationsentwickler", "IMS",
+    "Mediamaitker", "Recyclist", "Detailhandel", "Kaufmann"];
+
+let lernendenListe =
+    [
+        {
+            "kundenid": 101,
+            "firstname": "Hans",
+            "lastname": "Muster",
+            "adresse": {
+                "strasse": "Oberaurüti 1",
+                "ort": "Fischenthal",
+                "plz": "8497"
+            },
+            "courses": {
+                "berufskunde": ["M286", "M287", "M288", "usw."],
+                "allgemein": ["Franz", "Deutsch", "Mathe", "usw."]
+            }
+        }, {
+            "kundenid": 101,
+            "firstname": "Hans",
+            "lastname": "Muster",
+            "adresse": {
+                "strasse": "Oberaurüti 1",
+                "ort": "Fischenthal",
+                "plz": "8497"
+            },
+            "courses": {
+                "berufskunde": ["M286", "M287", "M288", "usw."],
+                "allgemein": ["Franz", "Deutsch", "Mathe", "usw."]
+            }
+        }, {
+            "kundenid": 101,
+            "firstname": "Hans",
+            "lastname": "Muster",
+            "adresse": {
+                "strasse": "Oberaurüti 1",
+                "ort": "Fischenthal",
+                "plz": "8497"
+            },
+            "courses": {
+                "berufskunde": ["M286", "M287", "M288", "usw."],
+                "allgemein": ["Franz", "Deutsch", "Mathe", "usw."]
+            }
+        },
+    ]
